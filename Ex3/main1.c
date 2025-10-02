68c56cf49d5b2ac638c18b0c8c6e096db9b6173c
lorder430 <lorder430@gmail.com>
2025-10-02 21:30:11 +0800

    Update main1.c

--- Ex3/main1.c
+++ Ex3/main1.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+void print_sp(int i, int n){
+    for(int sp = 0; sp < n-i; sp++){
+        printf(" ");
+    }
+}
+
+
+void print_num(int n){
+    for(int i = 0; i < n; i++){
+        printf("%d " , n);
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    int rows = 6;
+    for(int i =0;i <= rows;i++){
+        print_sp(i,rows);
+        print_num(i);
+    }
+    return 0;
+}
 

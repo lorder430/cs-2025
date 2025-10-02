440fca56b165adef962ba1134709997807e0d04e
lorder430 <lorder430@gmail.com>
2025-10-02 14:13:09 +0800

    Update main3.c

--- Ex3/main3.c
+++ Ex3/main3.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 2;
+    if (i % 15 == 0){
+        printf("Love Iu");
+    }
+    else if (i % 5 ==0){
+        printf("Iu");
+    }
+    else {
+        printf("%d\n", i);
+    }
+    return 0;
+}
 
